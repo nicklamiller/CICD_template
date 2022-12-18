ddf25b1811484a9a5bfb2a3f96f08cff4029325e
nicklamiller <nicklamiller@gmail.com>
2022-12-17 22:49:51 -0800

    add src folder for files to be linted

---
+++ src/my_module.py
@@ -0,0 +1,25 @@
+"""Put module description here."""
+
+
+def test_function1(
+    arg1,
+    arg2,
+    arg3,
+):
+    return sum(
+        arg1,
+        arg2,
+        arg3,
+    )
+
+
+def test_function2(
+    arg1,
+    arg2,
+    arg3,
+):
+    test_function1(
+        arg1,
+        arg2,
+        arg3,
+    )
