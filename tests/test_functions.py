6fba11f4a095f6512e69a7c500c238c94ce4c6ff
nicklamiller <nicklamiller@gmail.com>
2023-01-22 20:54:38 -0800

    add tests directory

---
+++ tests/test_functions.py
@@ -0,0 +1,9 @@
+"""This module is for testing functions in my_module."""
+from src import my_module
+
+
+def test_function1():
+    list_numbers = [2, 3, 5]
+    assert my_module.function1(
+        list_numbers,
+    ) == 10
