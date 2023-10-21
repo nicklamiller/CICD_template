cc0bab177bc4854e67c22b9477a0f4a0c8316c08
nicklamiller <nicklamiller@gmail.com>
2023-10-20 19:23:33 -0700

    flesh out docstring for example function

--- src/my_module.py
+++ src/my_module.py
@@ -3,4 +3,12 @@ from typing import List, Union
 
 
 def function1(list_numbers: List[Union[int, float]]) -> Union[int, float]:
+    """Sum a list of numbers.
+
+    Args:
+        list_numbers (List[int | float]): a list of numbers
+
+    Returns:
+        int | float: sum of the list of numbers
+    """
     return sum(list_numbers)
