fe096827cf6e9b58ebcb78b85177e738c7786b97
nicklamiller <nicklamiller@gmail.com>
2023-10-20 19:20:39 -0700

    add typehints to example my_module

--- src/my_module.py
+++ src/my_module.py
@@ -1,5 +1,6 @@
 """Put module description here."""
+from typing import List, Union
 
 
-def function1(list_numbers):
+def function1(list_numbers: List[Union[int, float]]) -> Union[int, float]:
     return sum(list_numbers)
